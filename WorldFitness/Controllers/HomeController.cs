1c7f954e4ab2e95976759dd83536fddee11d7c2e
annieuser2022 <aooj2016@gmail.com>
2022-12-20 06:47:02 -0700

    Login button works, but can't bring to admin dashboard

---
+++ WorldFitness/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WorldFitness.Models;
+
+namespace WorldFitness.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        static string adminPassword="admin";
+        static string adminId="admin@fitness.com";
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult LoginPage()
+        {
+            return View();
+        }
+        public IActionResult CheckCredentials(Login details) 
+        { 
+            if(details.EmailId == adminId && details.Password == adminPassword)
+            {
+                return RedirectToAction("AdminIndex", "Admin");
+            }
+            return View("LoginPage"); 
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ WorldFitness/Models/Login.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WorldFitness.Models
+{
+    public class Login 
+    {
+       public string? EmailId { get; set; }
+       public string? Password { get; set; }
+    }
+}
